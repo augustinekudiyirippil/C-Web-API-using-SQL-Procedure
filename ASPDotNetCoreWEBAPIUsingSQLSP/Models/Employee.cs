3d77ea6c2e87ac1fd36db3f262ec1052672a6f93
augustinekudiyirippil <augustinekudiyirippil@gmail.com>
2023-12-25 13:41:27 +0000

    Employee Model class added

---
+++ ASPDotNetCoreWEBAPIUsingSQLSP/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPDotNetCoreWEBAPIUsingSQLSP.Models
+{
+    public class Employee
+    {
+        public int ID { get; set;}
+        public string Name { get; set; }
+        public int Age  { get; set; }
+        public int Active { get; set; }
+
+
+    }
+}
