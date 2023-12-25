7181affd877537c759c68e4617ce15c87542c9c1
augustinekudiyirippil <augustinekudiyirippil@gmail.com>
2023-12-25 14:10:18 +0000

    Added Database connection string in webconfig file

--- ASPDotNetCoreWEBAPIUsingSQLSP/Controllers/ValuesController.cs
+++ ASPDotNetCoreWEBAPIUsingSQLSP/Controllers/ValuesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASPDotNetCoreWEBAPIUsingSQLSP.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,11 @@ namespace ASPDotNetCoreWEBAPIUsingSQLSP.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+
+        Employee emp = new Employee();
+
+
+
         // GET: api/<ValuesController>
         [HttpGet]
         public IEnumerable<string> Get()
